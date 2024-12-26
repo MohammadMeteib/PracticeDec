03a91e9aeea546dc67f3dc7b85c44ba250bcd1c0
ABK <ABK@DESKTOP-PTCS16K>
2024-12-26 10:16:35 +0300

    Add project files.

---
+++ PracticeDec/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeDec.Models
+{
+    public class Product
+    {
+        public string ProductInfo(string name)
+        {
+            return "Name of the product is: " + name;
+        }
+
+        public string ProductInfo(string name, int price)
+        {
+            return "Name of the product is: " + name + " and the price is: " + price;
+        }
+
+        public string ProductInfo(string name, int price, int quantity)
+        {
+            return "name of the product is: "+ name +". the price of the product is: "+price+". the quantity is: "+ quantity+ ". total is: " + (price * quantity);
+        }
+    }
+}
+    
+
+   
+    // function 1 product
+    // function 2 product and price
+    // function 3 product price and quantity
+   
+
---
+++ PracticeDec/Program.cs
@@ -0,0 +1,12 @@
+﻿using PracticeDec.
+    Models;
+Product product = new Product();
+Console.WriteLine("Enter product name:");
+string name = Console.ReadLine();
+Console.WriteLine(product.ProductInfo(name));
+Console.WriteLine("Enter product price");
+int price = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(product.ProductInfo(name, price));
+Console.WriteLine("Enter product quantity");
+int quantity = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(product.ProductInfo(name, price, quantity));
